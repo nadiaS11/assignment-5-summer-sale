0c810e22af07581ccc63a13bb7e807ec75348970
nadiaS11 <nadiasultana.ns11@gmail.com>
2023-08-20 21:11:36 +0600

    updated js file with clean code

--- code.js
+++ code.js
@@ -1,77 +1,75 @@
 let total = 0;
 let grandTotal = 0;
-let discounts= 0;
+let discounts = 0;
 const applyButton = document.getElementById("btn-apply");
 const discountInput = document.getElementById("discount-input");
+const totalAmount = document.getElementById("total-amount");
+const discountAmount = document.getElementById("discount-amount");
+const grandTotalAmount = document.getElementById("grand-amount");
+const cartItems = document.getElementById("selected-items");
+const purchaseButton = document.getElementById("btn-purchase");
 
-
-// To apply discount after total>200 and input is exactly SELL200
+// To apply discount after total > 200 and input is exactly SELL200
 function applyDiscount() {
-    if (total >= 200 && discountInput.value==='SELL200') {
-        updateDiscountAndGrandTotal();
-        
-    }
-    else{alert("Coupon code invalid")
-    discountInput.value=""
-    
-}
+  if (total >= 200 && discountInput.value === 'SELL200') {
+    updateDiscountAndGrandTotal();
+  } else {
+    alert("Coupon code invalid");
+    discountInput.value = "";
   }
-  
+}
+
 // For discount "Apply" button 
 applyButton.addEventListener("click", function() {
   applyDiscount();
-  
 });
 
-// to update the grand total after applying discounts
+// To update the discount and grand total after applying discounts
 function updateDiscountAndGrandTotal() {
-  if (total >= 200 && discountInput.value==='SELL200') {
-        discounts = total * 0.2;
-      } else {
-        discounts = 0;
-      }
-      const discountAmount = document.getElementById("discount-amount");
-      discountAmount.innerText = discounts.toFixed(2);
-    grandTotal = total - discounts;
-    const grandTotalAmount = document.getElementById("grand-amount");
-    grandTotalAmount.innerText = grandTotal.toFixed(2);
+  if (total >= 200 && discountInput.value === 'SELL200') {
+    discounts = total * 0.2;
+  } else {
+    discounts = 0;
   }
+  discountAmount.innerText = discounts.toFixed(2);
+  grandTotal = total - discounts;
+  grandTotalAmount.innerText = grandTotal.toFixed(2);
+}
 
-function whenClicked(e){
-    const cartItems = document.getElementById("selected-items");
-  const totalAmount = document.getElementById("total-amount");
 
-  //get the product name
+
+// To get name and totals with discount when clicked on products
+function whenClicked(e) {
   const getName = e.childNodes[3].childNodes[3].innerText;
   const li = document.createElement("li");
-    li.innerText = getName;
-    li.style.fontWeight = "600"
-    cartItems.appendChild(li)
-
-
-
+  li.innerText = getName;
+  li.style.fontWeight = "600";
+  cartItems.appendChild(li);
   const getPrice = parseFloat(e.childNodes[3].childNodes[5].innerText);
   total += getPrice;
   totalAmount.innerText = total.toFixed(2);
   if (total >= 200) {
-    document.getElementById("btn-apply").disabled = false;
+    applyButton.disabled = false;
   }
-  updateDiscountAndGrandTotal()
- 
-
-  const purchaseButton = document.getElementById("btn-purchase");
+  updateDiscountAndGrandTotal();
   if (total > 0) {
     purchaseButton.disabled = false;
   } else {
     purchaseButton.disabled = true;
   }
-
-    // console.log(discounts)
 }
 
+// to reset everything to initial value
+function resetAll() {
+    applyButton.disabled = true;
+    discountInput.value = "";
+    total = 0;
+    grandTotal = 0;
+    discounts = 0;
+    totalAmount.innerText = "00";
+    discountAmount.innerText = "00";
+    grandTotalAmount.innerText = "00";
+    cartItems.innerText = "";
+    purchaseButton.disabled = true;
+  }
 
- // To Go home page
- function goHome(e){
-  window.location.href='index.html'
- }
- 
