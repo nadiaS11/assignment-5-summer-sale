cac80ab1a81fb134346374ce662628bc69d4fb67
nadiaS11 <nadiasultana.ns11@gmail.com>
2023-08-20 14:39:32 +0600

    changes in grandtotal and discount function

--- code.js
+++ code.js
@@ -7,12 +7,13 @@ const discountInput = document.getElementById("discount-input");
 
 // To apply discount after total>200 and input is exactly SELL200
 function applyDiscount() {
-    if (total > 200 && discountInput.value==='SELL200') {
-      discounts = total * 0.2;
-      const discountAmount = document.getElementById("discount-amount");
-      discountAmount.innerText = discounts.toFixed(2);
-      updateGrandTotal();
+    if (total >= 200 && discountInput.value==='SELL200') {
+        updateDiscountAndGrandTotal();
     }
+    else{alert("Coupon code invalid")
+    discountInput.value=""
+    
+}
   }
   
 // For discount "Apply" button 
@@ -21,29 +22,41 @@ applyButton.addEventListener("click", function() {
   applyButton.disabled = true;
 });
 
+// to update the grand total after applying discounts
+function updateDiscountAndGrandTotal() {
+    if (total >= 200) {
+        discounts = total * 0.2;
+      } else {
+        discounts = 0;
+      }
+      const discountAmount = document.getElementById("discount-amount");
+      discountAmount.innerText = discounts.toFixed(2);
+    grandTotal = total - discounts;
+    const grandTotalAmount = document.getElementById("grand-amount");
+    grandTotalAmount.innerText = grandTotal.toFixed(2);
+  }
+
 function whenClicked(e){
     const cartItems = document.getElementById("selected-items");
   const totalAmount = document.getElementById("total-amount");
-  const grandTotalAmount = document.getElementById("grand-amount");
-  const discountAmount = document.getElementById("discount-amount");
-  const getPrice = parseFloat(e.childNodes[3].childNodes[5].innerText);
 
   //get the product name
   const getName = e.childNodes[3].childNodes[3].innerText;
   const li = document.createElement("li");
     li.innerText = getName;
-    li.style.fontWeight = "500"
+    li.style.fontWeight = "600"
     cartItems.appendChild(li)
 
-  //set the total and grand total
-  total = parseFloat(total) + parseFloat(getPrice);
-  discounts= total*0.2
-  discountAmount.innerText = discounts.toFixed(2)
 
+
+  const getPrice = parseFloat(e.childNodes[3].childNodes[5].innerText);
+  total += getPrice;
   totalAmount.innerText = total.toFixed(2);
-  grandTotal = total - discounts;
-  grandTotalAmount.innerText = grandTotal.toFixed(2);
-
+  if (total >= 200) {
+    document.getElementById("btn-apply").disabled = false;
+  }
+  updateDiscountAndGrandTotal()
+ 
 
   const purchaseButton = document.getElementById("btn-purchase");
   if (total > 0) {
